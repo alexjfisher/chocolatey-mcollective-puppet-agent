cb60513e6a67eb95bac2417996ea11f392cfce6f
Alexander Fisher <alex@linfratech.co.uk>
2016-10-07 12:19:12 +0100

    initial commit

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$packageName= 'mcollective-puppet-agent'
+$version = '1.11.1'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$pluginDir = 'C:\Program Files\Puppet Labs\Puppet\mcollective\lib\mcollective'
+
+Get-ChocolateyUnzip -FileFullPath "$toolsDir\mcollective-puppet-agent-$version.zip" -Destination $toolsDir
+
+$installedFiles = @()
+ForEach ($dir in 'agent','aggregate','application','data','util','validator') {
+  New-Item -ItemType Directory -Force -Path $pluginDir\$dir
+  $files = Copy-Item $toolsDir\mcollective-puppet-agent-$version\$dir\* $pluginDir\$dir -recurse -force -PassThru -ErrorAction silentlyContinue
+  if($files) {
+    $installedFiles += $files
+  } else { "Copy failure"}
+}
+$installedFiles | select -exp FullName | set-content $toolsDir\installed_files.log
+
+get-service mcollective | restart-service -force
---
+++ tools/chocolateyuninstall.ps1
@@ -0,0 +1,9 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = 'C:\ProgramData\chocolatey\lib\mcollective-puppet-agent\tools' #"$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installedFiles = gc $toolsDir\installed_files.log
+$installedFiles | ?{$_.trim() -ne '' -and (test-path $_)} | %{
+  gci $_ | ?{-not $_.PSIsContainer} | %{
+    Remove-Item $_.FullName -force
+  }
+}
+get-service mcollective | restart-service -force
